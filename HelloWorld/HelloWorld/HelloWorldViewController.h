94b826684244875ce9fb5166b0eccdc541755080
hughryan <mcdonalh@onid.orst.edu>
2014-01-18 14:36:02 -0800

    M1: Hello World Spike

---
+++ HelloWorld/HelloWorld/HelloWorldViewController.h
@@ -0,0 +1,15 @@
+//
+//  HelloWorldViewController.h
+//  HelloWorld
+//
+//  Created by Hugh McDonald on 1/18/14.
+//  Copyright (c) 2014 snowFish. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HelloWorldViewController : UIViewController
+
+-(IBAction)showMessage;
+
+@end
