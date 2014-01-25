70fa6a78795999a6c48849720261dcb9998b4d36
hughryan <mcdonalh@onid.orst.edu>
2014-01-24 18:56:03 -0800

    Initial Delegate created.

---
+++ Time Traveler/Time Traveler/TimeTravelerAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TimeTravelerAppDelegate.h
+//  Time Traveler
+//
+//  Created by Hugh McDonald on 1/24/14.
+//  Copyright (c) 2014 snowFish. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TimeTravelerAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Time Traveler/Time TravelerTests/Time_TravelerTests.m
@@ -0,0 +1,34 @@
+//
+//  Time_TravelerTests.m
+//  Time TravelerTests
+//
+//  Created by Hugh McDonald on 1/24/14.
+//  Copyright (c) 2014 snowFish. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Time_TravelerTests : XCTestCase
+
+@end
+
+@implementation Time_TravelerTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
