22f1a84cf92d8f90b69f2d8c99c25684e80b1954
hughryan <mcdonalh@onid.orst.edu>
2014-02-15 12:12:30 -0800

    Fixed CoreData integration.

---
+++ TimeTraveler/TimeTraveler/Model/TimeTravelerTripDetails.m
@@ -0,0 +1,20 @@
+//
+//  TimeTravelerTripDetails.m
+//  TimeTraveler
+//
+//  Created by Hugh McDonald on 2/15/14.
+//  Copyright (c) 2014 snowFish. All rights reserved.
+//
+
+#import "TimeTravelerTripDetails.h"
+
+
+@implementation TimeTravelerTripDetails
+
+@dynamic destinationLocation;
+@dynamic departureDate;
+@dynamic wakeTime;
+@dynamic sleepTime;
+@dynamic notificationsSetting;
+
+@end
